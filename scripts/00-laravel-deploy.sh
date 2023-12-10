d1ae11255f62810575dbdc5d909a9d0dca418aed
passionate-code <60539975+passionate-code@users.noreply.github.com>
2023-12-11 07:35:05 +0800

    #27

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -2,6 +2,7 @@
 echo "Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
+composer require laravel/framework:6.0.0
 
 #echo "generating application key..."
 #php artisan key:generate --show
