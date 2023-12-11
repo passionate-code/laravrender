216eebaf4f1874dabff3cb48700dcbb1435ea937
passionate-code <60539975+passionate-code@users.noreply.github.com>
2023-12-11 23:31:28 +0800

    #48

--- app/User.php
+++ app/User.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
@@ -28,45 +29,11 @@ class User extends Authenticatable
     ];
 
     /**
-     * Add a mutator to ensure hashed passwords
-     */
-    public function setPasswordAttribute($password)
-    {
-        $this->attributes['password'] = bcrypt($password);
-    }
-}
-
-<?php
-
-
-
-class User extends Authenticatable
-{
-    use Notifiable;
-    
-    /**
-     * The attributes that are mass assignable.
+     * The attributes that should be cast to native types.
      *
      * @var array
      */
-    protected $fillable = [
-        'name', 'email', 'password',
+    protected $casts = [
+        'email_verified_at' => 'datetime',
     ];
-    
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
-    
-    /**
-     * Add a mutator to ensure hashed passwords
-     */
-    public function setPasswordAttribute($password)
-    {
-        $this->attributes['password'] = bcrypt($password);
-    }
 }
--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -2,7 +2,6 @@
 echo "Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
-pwd
 mv Parser.php /var/www/html/vendor/egulias/email-validator/EmailValidator/Parser
 
 #echo "generating application key..."
